e23125fa1659c1d92ea8322e5d8d6ed7f4a0952c
lynch0227 <kevin.yao@aliyun.com>
2020-01-31 21:47:50 +0800

    add groutine and share mem test

--- src/series/series.go
+++ src/series/series.go
@@ -1,5 +1,12 @@
 package series
 
+import "fmt"
+
+func init() {
+	fmt.Println("init function")
+}
+
+//GetFibonacciSerie Function
 func GetFibonacciSerie(n int) []int {
 	ret := []int{1, 1}
 	for i := 2; i < n; i++ {
