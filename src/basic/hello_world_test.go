60e94b1da89e22d5c0fe90bcf91e11225f0136c3
lynch0227 <kevin.yao@aliyun.com>
2020-09-28 22:00:38 +0800

    增加打印varchar空值

--- src/basic/hello_world_test.go
+++ src/basic/hello_world_test.go
@@ -1,11 +1,18 @@
 package basic
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
+func variables() {
+	aa, bb := 1, ""
+	fmt.Printf("%d %q\n", aa, bb)
+}
+
 func TestHelloWorld(t *testing.T) {
+	variables()
 	if len(os.Args) > 1 {
 		t.Log("Hello World!", os.Args[1])
 	}
